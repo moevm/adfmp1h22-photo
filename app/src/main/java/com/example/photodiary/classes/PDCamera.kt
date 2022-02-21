27593038d9917c05d423a04667300e39de8f811f
TheShenk <udod_maksim@mail.ru>
2022-02-21 23:15:50 +0300

    feat: proper work with camera and files for it

---
+++ app/src/main/java/com/example/photodiary/classes/PDCamera.kt
@@ -0,0 +1,33 @@
+package com.example.photodiary.classes
+
+import android.content.Context
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+
+import androidx.core.content.FileProvider
+import java.io.File
+
+class PDCamera(private val activity: ComponentActivity): ActivityResultCallback<Boolean> {
+
+    private var imageFile = File(activity.filesDir, "default_image.jpg")
+    private val activityLauncher = activity.registerForActivityResult(ActivityResultContracts.TakePicture(), this)
+
+    fun open() {
+        imageFile = File(activity.filesDir, "test2.jpg")
+
+        val fileUri = FileProvider.getUriForFile(
+            activity,
+            "com.example.photodiary.provider",
+            imageFile
+        )
+
+        activityLauncher.launch(fileUri)
+    }
+
+    override fun onActivityResult(result: Boolean?) {
+        Log.d("PHOTO_DIARY", "Result: $result")
+    }
+
+}
