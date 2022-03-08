d312bfb700ca6a7b78ba736990ca7b8acb5ecfa6
Yogik10 <yogik_10@bk.ru>
2022-03-08 18:17:46 +0300

    Photo sharing added

--- app/src/main/java/com/example/photodiary/Photo.kt
+++ app/src/main/java/com/example/photodiary/Photo.kt
@@ -2,19 +2,21 @@ package com.example.photodiary
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.os.StrictMode
+import android.os.StrictMode.VmPolicy
 import android.util.Log
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.photodiary.classes.Day
 import com.example.photodiary.classes.EditDialog
 import com.example.photodiary.classes.PDDB
 import com.example.photodiary.classes.RemoveDialog
 import java.io.File
-import java.util.*
+
 
 class Photo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,10 +36,11 @@ class Photo : AppCompatActivity() {
         val descriptionView: TextView = findViewById(R.id.description)
         descriptionView.text = photoInfo?.description
 
+        val storageDir = this.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val imageFile = File(storageDir, photoInfo?.fileName)
+
         val deleteButton: ImageButton = findViewById(R.id.delete)
         deleteButton.setOnClickListener{
-            val storageDir = this.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-            val imageFile = File(storageDir, photoInfo?.fileName)
             val day = intent.getSerializableExtra("day") as Day
             val removeDialog = RemoveDialog(photoInfo?.id, imageFile, day)
             val manager = supportFragmentManager
@@ -54,12 +57,18 @@ class Photo : AppCompatActivity() {
 
         val shareButton: ImageButton = findViewById(R.id.share)
         shareButton.setOnClickListener{
-            val shareIntent: Intent = Intent().apply {
-                action = Intent.ACTION_SEND
-                putExtra(Intent.EXTRA_STREAM, "@mipmap/img")
-                type = "image/*"
-            }
-            startActivity(Intent.createChooser(shareIntent, null))
+//            val shareIntent = Intent(Intent.ACTION_SEND)
+//            shareIntent.type = "image/jpg"
+//            shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(Environment.getExternalStorageDirectory().path + "/Android/data/com.example.photodiary/files/Pictures/" + photoInfo?.fileName))
+//            startActivity(Intent.createChooser(shareIntent, "Share image using"))
+
+            val builder = VmPolicy.Builder()
+            StrictMode.setVmPolicy(builder.build())
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "image/jpg"
+            shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(imageFile))
+            Log.d("TAG", Uri.fromFile(imageFile).toString())
+            startActivity(Intent.createChooser(shareIntent, "Share link using"))
         }
     }
 }
