f06e159ada2e9bf1f938329cd0deb8da87d8d790
TheShenk <udod_maksim@mail.ru>
2022-03-15 16:54:37 +0300

    feat: test edit text

--- app/src/androidTest/java/com/example/photodiary/AddPhotoTest.kt
+++ app/src/androidTest/java/com/example/photodiary/AddPhotoTest.kt
@@ -8,11 +8,14 @@ import android.graphics.BitmapFactory
 import android.provider.MediaStore
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.core.internal.deps.guava.collect.Iterables
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -45,6 +48,12 @@ class AddPhotoTest {
         onView(withId(R.id.navigation_camera)).perform(click())
         val receivedIntent: Intent = Iterables.getFirst(Intents.getIntents(), Intent())
         assertThat(receivedIntent).hasAction(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        onView(withId(R.id.descriptionTextEdit)).perform(
+            ViewActions.clearText(),
+            ViewActions.typeText("Hello World!"),
+            ViewActions.closeSoftKeyboard()
+        ).check(ViewAssertions.matches(ViewMatchers.withText("Hello World!")))
     }
 
 }
